a5d9260bfbd8b0353d85cd20702109b8ef16624f
devz3ro <devz3ro@gmail.com>
2025-09-27 23:31:43 -0400

    Add files via upload
    Twin Peaks bug fixes

--- PVPSound_Compat.lua
+++ PVPSound_Compat.lua
@@ -54,3 +54,18 @@ function PVPS_OpenColorPicker(info)
         return OpenColorPicker(info)
     end
 end
+
+
+-- Wrapper for battleground flag position across API versions.
+if not PVPS_GetBattlefieldFlagPosition then
+    function PVPS_GetBattlefieldFlagPosition(index)
+        local uiMapId = C_Map and C_Map.GetBestMapForUnit and C_Map.GetBestMapForUnit("player") or nil
+        if C_PvP and C_PvP.GetBattlefieldFlagPosition and uiMapId then
+            local x, y, token = C_PvP.GetBattlefieldFlagPosition(index, uiMapId)
+            return x, y, token
+        elseif _G.GetBattlefieldFlagPosition then
+            return _G.GetBattlefieldFlagPosition(index)
+        end
+        return nil, nil, nil
+    end
+end
